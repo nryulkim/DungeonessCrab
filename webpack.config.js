cc5f0a5eb26809372522b0016a9f00bdb4c5d968
Nam Kim <nryulkim@gmail.com>
2016-10-21 11:53:58 -0400

    Fix bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: "./lib/dcrabs.js",
   output: {
-    path: path.join(__dirname),
+    path: path.join(__dirname, 'lib'),
     filename: "bundle.js"
   },
   module: {
